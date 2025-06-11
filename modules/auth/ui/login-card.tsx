61e2c6ed7d25d240bb6abf53739664f5ec4182d2
Achref Mehri <ashrefmehri24@gmail.com>
2025-06-11 22:34:53 +0200

    text

--- modules/auth/ui/login-card.tsx
+++ modules/auth/ui/login-card.tsx
@@ -5,8 +5,7 @@ export const LoginCard =()=>{
         <div className="flex flex-col gap-6 ">
             <Card className="overflow-hidden p-0">
             <CardContent className="grid md:grid-cols-2 p-0 ">
-                <p>0lr;v</p>
-                <p>258888888886</p>
+                Hellooo please login 
             </CardContent>
 
         </Card>
