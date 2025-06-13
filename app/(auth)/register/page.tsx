3b480e6f0006e287ab96b57b75bd6805d2cee1df
Achref Mehri <ashrefmehri24@gmail.com>
2025-06-13 13:39:13 +0200

    03: credentials auth

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
-import { RegisterCard } from "@/modules/auth/ui/register-card";
+import { RegisterForm } from "@/modules/auth/ui/register-card";
 
 export default function RegisterPage() {
-  return <RegisterCard />;
+  return <RegisterForm />;
 }
